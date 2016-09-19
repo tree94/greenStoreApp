3cb2e0e31c18cbb563222a57eaceaa20b7110ca8
JINSPIRATION\X <6_678@naver.com>
2016-09-19 20:51:12 +0900

    no message

---
+++ GreenStoreApp/app/src/main/java/com/seoul/greenstore/greenstore/NoticeActivity.java
@@ -0,0 +1,35 @@
+package com.seoul.greenstore.greenstore;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class NoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+//        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+    }
+
+
+
+
+
+}
