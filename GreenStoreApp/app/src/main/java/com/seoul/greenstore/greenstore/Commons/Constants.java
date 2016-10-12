c15fc7c40862e7c90df5131a0e8dd89cc6889c5f
mkh9293 <mkh9293@gmail.com>
2016-10-12 17:42:23 +0900

    상세페이지에 다음 지도 추가

--- GreenStoreApp/app/src/main/java/com/seoul/greenstore/greenstore/Commons/Constants.java
+++ GreenStoreApp/app/src/main/java/com/seoul/greenstore/greenstore/Commons/Constants.java
@@ -5,17 +5,13 @@ package com.seoul.greenstore.greenstore.Commons;
  */
 public class Constants {
 
-//        public static final String BASIC_URL = "http://192.168.100.247:8080/";
+        public static final String BASIC_URL = "http://192.168.100.253:8080/";
 //    public static final String BASIC_URL = "http://172.30.49.177:8080/";
 
-    public static final String BASIC_URL = "http://192.168.100.253:8080/";
+//    public static final String BASIC_URL = "http://192.168.13.36:8080/";
 //    public static final String BASIC_URL = "http://172.30.49.177:8080/";
 
-//    public static final String BASIC_URL = "http://192.168.100.253:8080/";
-//    public static final String BASIC_URL = "http://192.168.100.247:8080/";
 
-//    public static final String BASIC_URL = "http://172.30.40.131:8080/";
-//    public static final String BASIC_URL = "http://172.30.40.131:8080/";
 
     public static final String GREEN_STORE_URL_BASIC = BASIC_URL + "greenStore/";
     public static final String GREEN_STORE_URL_APP = GREEN_STORE_URL_BASIC + "app/";
---
+++ GreenStoreApp/app/src/main/java/com/seoul/greenstore/greenstore/Commons/MapKeys.java
@@ -0,0 +1,8 @@
+package com.seoul.greenstore.greenstore.Commons;
+
+/**
+ * Created by user on 2016-10-11.
+ */
+public class MapKeys {
+    public static final String daumMapKey = "831f28db158ecea7cdafb95c96089175";
+}
